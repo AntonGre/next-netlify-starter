ffd176a9eb6f127ee0f772fcc0aefdeb887d6b72
Anton <anton@vembyte.dk>
2022-09-30 15:35:27 +0200

    asd

--- .github/scripts/environments.js
+++ .github/scripts/environments.js
@@ -1,9 +1,3 @@
 "use strict";
 
-// const core = require("@actions/core");
-// const github = require("@actions/github");
-
 console.log("AUTH_TOKEN", process.env.NETLIFY_AUTH_TOKEN);
-console.log("LABELS", process.env.GITHUB_LABELS);
-
-// core.console.log(JSON.stringify(github.context, undefined, 2));
