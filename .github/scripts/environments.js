f0d0a4990e318e5b9d0830ab5cdb32899ff6f10b
Anton <anton@vembyte.dk>
2022-09-30 15:54:59 +0200

    asd

--- .github/scripts/environments.js
+++ .github/scripts/environments.js
@@ -7,7 +7,8 @@ console.log("AUTH_TOKEN", process.env.NETLIFY_AUTH_TOKEN);
 
 (async () => {
   await exec("netlify link --id '08a94377-dc9d-4a7e-a460-d465edb91e12'");
-  await exec("netlify env:list");
+  const test = await exec("netlify env:list --json");
+  console.log(test);
 })()
   .then(() => {
     console.log("done");
