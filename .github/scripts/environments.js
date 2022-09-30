baf4428c54eed2d13c2fd78b79faebdc7184a98b
Anton <anton@vembyte.dk>
2022-09-30 16:19:11 +0200

    asd

--- .github/scripts/environments.js
+++ .github/scripts/environments.js
@@ -5,11 +5,15 @@ const exec = util.promisify(require("child_process").exec);
 
 (async () => {
   await exec("netlify link --id '08a94377-dc9d-4a7e-a460-d465edb91e12'");
-  const netEnvs = await exec("netlify env:list --context production  --json");
+  const { stdout } = await exec(
+    "netlify env:list --context production  --json"
+  );
+
+  const netEnvs = JSON.parse(stdout);
 
   // set branch specific to env names
   for (let [key, value] of Object.entries(netEnvs)) {
-    await exec(`netlify env:set ${key} ${value} --context branch`);
+    await exec(`netlify env:set ${key} '${value}' --context branch`);
   }
 })()
   .then(() => {
@@ -17,4 +21,5 @@ const exec = util.promisify(require("child_process").exec);
   })
   .catch((e) => {
     console.error(e);
+    return -1;
   });
