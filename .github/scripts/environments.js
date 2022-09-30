6c69d75ee50b16765583fc612b72529d28c5bb46
Anton <anton@vembyte.dk>
2022-09-30 16:14:37 +0200

    env.js

--- .github/scripts/environments.js
+++ .github/scripts/environments.js
@@ -1,15 +1,16 @@
 "use strict";
 
-console.log("aaa");
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
-console.log("AUTH_TOKEN", process.env.NETLIFY_AUTH_TOKEN);
-
 (async () => {
   await exec("netlify link --id '08a94377-dc9d-4a7e-a460-d465edb91e12'");
-  const test = await exec("netlify env:list --json");
-  console.log(test);
+  const netEnvs = await exec("netlify env:list --context production  --json");
+
+  // set branch specific to env names
+  for (let [key, value] of Object.entries(netEnvs)) {
+    await exec(`netlify env:set ${key} ${value} --context branch`);
+  }
 })()
   .then(() => {
     console.log("done");
