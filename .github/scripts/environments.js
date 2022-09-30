f37ba59b551b84c56dc803bc9e693a252f314226
Anton <anton@vembyte.dk>
2022-09-30 15:05:40 +0200

    ASD

--- .github/scripts/environments.js
+++ .github/scripts/environments.js
@@ -3,5 +3,7 @@
 // const core = require("@actions/core");
 // const github = require("@actions/github");
 
-console.log(process.env.NETLIFY_AUTH_TOKEN);
+console.log("AUTH_TOKEN", process.env.NETLIFY_AUTH_TOKEN);
+console.log("LABELS", process.env.GITHUB_LABELS);
+
 // core.console.log(JSON.stringify(github.context, undefined, 2));
