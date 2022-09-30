5144780a2e8437981b210b891367fda9b2064c86
Anton <anton@vembyte.dk>
2022-09-30 15:51:06 +0200

    asdsa

--- .github/scripts/environments.js
+++ .github/scripts/environments.js
@@ -1,9 +1,12 @@
 "use strict";
+
+const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
 console.log("AUTH_TOKEN", process.env.NETLIFY_AUTH_TOKEN);
 
 (async () => {
+  await exec("netlify link --id '08a94377-dc9d-4a7e-a460-d465edb91e12'");
   await exec("netlify env:list");
 })()
   .then(() => {
