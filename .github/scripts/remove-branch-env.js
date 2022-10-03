d78ded8ebb28fe9a8c7e41a7e60a24abfa7d4653
Anton <anton@vembyte.dk>
2022-10-03 09:43:12 +0200

    asd

--- .github/scripts/remove-branch-env.js
+++ .github/scripts/remove-branch-env.js
@@ -15,10 +15,31 @@ if (!BRANCH || !SITE_ID) {
   const { stdout } = await exec(`netlify env:list --context ${BRANCH} --json`);
   const envs = JSON.parse(stdout);
 
-  // remove
+  // remove branch specific envs
   console.log(`Removing ${Object.keys(envs).length} envs...`);
   for (let [key, value] of Object.entries(envs)) {
     await exec(`netlify env:unset ${key} --context '${BRANCH}'`);
+  }
+
+  const siteResponse = await exec(
+    `netlify api getSite --data '${JSON.stringify({ siteId: SITE_ID })}' --json`
+  );
+  const siteData = JSON.parse(siteResponse.stdout);
+  const allowedBranches = siteData.build_settings.allowed_branches;
+
+  // remove branch to build settings
+  if (allowedBranches.contains(BRANCH)) {
+    allowedBranches.splice(allowedBranches.indexOf(BRANCH), 1);
+    await exec(
+      `netlify api updateSite --data '${JSON.stringify({
+        siteId: SITE_ID,
+        body: {
+          build_settings: {
+            allowed_branches: allowedBranches,
+          },
+        },
+      })}'`
+    );
   }
 })()
   .then(() => {
--- .github/scripts/set-branch-env.js
+++ .github/scripts/set-branch-env.js
@@ -17,11 +17,43 @@ if (!BRANCH || !SITE_ID) {
   );
   const productEnvs = JSON.parse(stdout);
 
-  // set branch specific env
+  // set production env to branch specific env
   console.log(`Adding ${Object.keys(productEnvs).length} envs...`);
   for (let [key, value] of Object.entries(productEnvs)) {
     await exec(`netlify env:set ${key} '${value}' --context '${BRANCH}'`);
   }
+
+  const siteResponse = await exec(
+    `netlify api getSite --data '${JSON.stringify({ siteId: SITE_ID })}' --json`
+  );
+
+  const siteData = JSON.parse(siteResponse.stdout);
+
+  // add branch to build settings
+  if (!siteData.build_settings.allowed_branches.contains(BRANCH)) {
+    await exec(
+      `netlify api updateSite --data '${JSON.stringify({
+        siteId: SITE_ID,
+        body: {
+          build_settings: {
+            allowed_branches: siteData.build_settings.allowed_branches.concat([
+              BRANCH,
+            ]),
+          },
+        },
+      })}'`
+    );
+  }
+
+  // Branch deploy
+  await exec(
+    `netlify api createSiteDeploy --data '${JSON.stringify({
+      siteId: SITE_ID,
+      body: {
+        branch: BRANCH,
+      },
+    })}'`
+  );
 })()
   .then(() => {
     console.log("done");
