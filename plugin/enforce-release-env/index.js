0bb46d17a89917bf7ef16089e1100a52ef28ba51
Anton <anton@vembyte.dk>
2022-10-10 11:21:59 +0200

    ASD

--- plugin/enforce-release-env/index.js
+++ plugin/enforce-release-env/index.js
@@ -7,16 +7,18 @@ module.exports = {
     const { CONTEXT } = process.env;
     const { SITE_ID, IS_LOCAL } = constants;
 
-    if (CONTEXT !== "deploy-preview") {
-      console.log("context is not deploy-preview existing...");
-      return;
-    }
+    console.log(CONTEXT);
+
+    // if (CONTEXT !== "deploy-preview") {
+    //   console.log("context is not deploy-preview existing...");
+    //   return;
+    // }
 
     const { stdout } = await run.command(
       "netlify env:list --context deploy-preview --json"
     );
     const envs = JSON.parse(stdout);
     Object.entries(envs).map(([key, value]) => setEnvValue(key, value));
-    console.log("Setup enviorment variables ");
+    console.log("Setup environment variables");
   },
 };
